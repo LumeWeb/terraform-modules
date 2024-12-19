77c21e008f09737a9d4090ded9c320a05fb8a56f
Derrick Hammer <derrick@derrickhammer.com>
2024-12-19 00:00:37 -0500

    refactor: update portal outputs

--- modules/service/portal/outputs.tf
+++ modules/service/portal/outputs.tf
@@ -1,4 +1,9 @@
-output "service_uri" {
-  description = "URIs where the portal service is accessible"
+output "dns_fqdn" {
+  description = "Portal service FQDN endpoint"
   value       = module.portal_deployment.dns_fqdn
+}
+
+output "ip_address" {
+  description = "Portal service IP address"
+  value       = module.portal_deployment.ips[0]
 }
