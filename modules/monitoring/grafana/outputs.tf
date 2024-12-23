69cb8513d7a47de5fff83fa2f23258bef27780e0
Derrick Hammer <derrick@derrickhammer.com>
2024-12-23 02:29:11 -0500

    refactor: add port and provider_host output for grafana

--- modules/monitoring/grafana/outputs.tf
+++ modules/monitoring/grafana/outputs.tf
@@ -2,3 +2,13 @@ output "dns_fqdn" {
   description = "Grafana DNS FQDN"
   value       =  module.grafana_deployment.dns_fqdn
 }
+
+output "port" {
+  description = "Port for the Grafana service"
+  value       = module.grafana_deployment.port
+}
+
+output "provider_host" {
+  description = "Provider host for the Grafana deployment"
+  value       = module.grafana_deployment.provider_host
+}
