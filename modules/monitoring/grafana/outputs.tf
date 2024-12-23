bc00a95793ea054502850f97258891d2ea294fe7
Derrick Hammer <derrick@derrickhammer.com>
2024-12-23 01:38:47 -0500

    refactor: change grafana outputs

--- modules/monitoring/grafana/outputs.tf
+++ modules/monitoring/grafana/outputs.tf
@@ -1,24 +1,4 @@
-output "deployment_id" {
-  description = "The ID of the Grafana Akash deployment"
-  value       = module.grafana_deployment.id
-}
-
-output "service_endpoints" {
-  description = "Map of Grafana service endpoints"
-  value       = module.grafana_deployment.service_endpoints
-}
-
-output "grafana_url" {
-  description = "URL to access Grafana UI"
-  value       = "https://${var.dns.domain}"
-}
-
-output "admin_user" {
-  description = "Grafana admin username"
-  value       = var.admin_user
-}
-
-output "state" {
-  description = "Current state of the Grafana deployment"
-  value       = module.grafana_deployment.state
+output "dns_fqdn" {
+  description = "Grafana DNS FQDN"
+  value       =  module.grafana_deployment.dns_fqdn
 }
